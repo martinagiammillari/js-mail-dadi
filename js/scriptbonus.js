be1b752d13653b3ac8fe348a32edde1f6cb6f63b
martinagiammillari <martigiammi@gmail.com>
2025-10-30 15:34:56 +0100

    feat: snack 2 done

--- js/scriptbonus.js
+++ js/scriptbonus.js
@@ -5,10 +5,52 @@
 // 2.se % 2 === 0 console log di numero
 // 3.se % 2 !== = console log di numero +1 
 
-const numero = parseInt(prompt("Inserisci un numero"));
-if (numero % 2 === 0) {
-    console.log(numero);
-}
-else if (numero % 2 !== 0) {
-    console.log(numero + 1);
-}
+//--------------------------------------------------------------------------------------
+// const numero = parseInt(prompt("Inserisci un numero"));
+// if (numero % 2 === 0) {
+//     console.log(numero);
+// }
+// else if (numero % 2 !== 0) {
+//     console.log(numero + 1);
+// }
+//--------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Snack 2
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// 1. conts wordOne, faccio prompt
+// 2. conts wordTwo, faccio prompt
+// 3. if wordOne > wordTwo console.log (wordTwo , wordOne)
+// 4. if wordOne < wordTwo console.log (wordOne , wordTwo)
+// 5. if wordOne === wordTwo console.log( "stessa lunghezza, nessun vincitore" )
+
+//--------------------------------------------------------------------------------------
+
+// const wordOne = prompt("inserisci una parola");
+// const wordTwo = prompt("inserisci una parola");
+// if (wordOne > wordTwo) {
+//     console.log(wordTwo, wordOne);
+// }
+// else if (wordOne < wordTwo) {
+//     console.log(wordOne, wordTwo);
+// }
+// else if (wordOne === wordTwo) {
+//     console.log("stessa lunghezza, nessun vincitore");
+// }
+
+//--------------------------------------------------------------------------------------
