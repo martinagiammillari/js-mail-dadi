f9894a0d89ff2c611c4dbdfb0b9643f63f7707dd
martinagiammillari <martigiammi@gmail.com>
2025-10-30 14:44:58 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Crea un array con delle email di chi può accedere.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Non è necessario provvedere alla validazione delle email.
+// NON USARE INCLUDES!
+
+// ESECUZIONE LOGICA
+// 1.crea array con dentro delle mail
+// 2.creo ciclo for con tanti cicli quante mail
+// 3.fai prompt dove cheido a utent di inserire una mail (dentro a ciclo for)
+// 4.if promptMail === mail, stampo messaggio
+
+
